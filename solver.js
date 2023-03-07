2b4be986387a503632d4634dba7ebd1d2246393a
gonnagetbetter <denissss906@gmail.com>
2023-03-07 08:56:50 +0200

    added solver and checking for discriminant values and roots which depend on it

--- solver.js
+++ solver.js
@@ -1,9 +1,25 @@
 'use strict';
 
-const discriminant = (a, b, c) => b * b - 4 * a * c;
+const getDiscriminant = (a, b, c) => b * b - 4 * a * c;
 
 const getRoots = (a, b, discriminant) => {
   const sqrt = Math.sqrt(discriminant);
-  const roots = [( - b + sqrt) / ( 2 * a ),(( - b - sqrt) / ( 2 * a ) )]
+  const roots = [( - b + sqrt) / ( 2 * a ), (( - b - sqrt) / ( 2 * a ) )];
   return roots;
 };
+
+const solver = (a, b, c) => {
+  const discriminant = getDiscriminant(a, b, c);
+  if (discriminant < 0) {
+    return 'There are no roots';
+  } else if ( discriminant === 0) {
+    console.log('Discriminant = 0. There are one root!');
+    const x1 = -b / 2 *a;
+    return x1 = ${x1};
+  }
+  console.log('There are two roots!');
+  const [x1, x2] = getRoots(a, b, discriminant);
+  return x1 = ${x1}\nx2 = ${x2};
+}
+
+export {quadraticEquationSolver};
