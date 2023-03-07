312a2bdd9469608aef3fdb0ab8292f9a51f98db6
gonnagetbetter <denissss906@gmail.com>
2023-03-07 08:47:26 +0200

    added spliting args func and cheking for a != 0

--- filemode.js
+++ filemode.js
@@ -10,13 +10,19 @@ const initFileMode = () => {
     console.log('Error. File ${path} does not exist\n');
     process.exit(1);
   }
-  
+
   const data = readFileSync(pathToFile, 'utf8');
 
   if (!/^-?\d+(.\d+)? -?\d+(.\d+)? -?\d+(.\d+)?\r?\n$/g.test(data)) {
     process.stdout.write('Error. Invalid file structure\n');
     process.exit(1);
   };
+
+  const [a, b, c] = data.split(' ').map((value) => parseFloat(value));
+
+  if (a === 0) {
+    process.stdout.write('Error. Value a cannot be 0\n');
+    process.exit(1);
   
 };
 
